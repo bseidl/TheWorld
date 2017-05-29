65d9466f6fe8d30ccc06257387979df685611c62
Bernhard Seidl <burzl-dev@online.de>
2017-05-29 14:44:26 +0200

    added javascript

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); // $ signals that the object is a jquery object (naming convention)
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
